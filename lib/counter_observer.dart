cf09dee1e1df6b87f2d6ab1418aeb960bcd86e26
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-17 12:19:07 -0700

    added logging statement

--- lib/counter_observer.dart
+++ lib/counter_observer.dart
@@ -13,6 +13,6 @@ class CounterObserver extends BlocObserver {
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
     // ignore: avoid_print
-    print('${bloc.runtimeType} $change');
+    print('RUNTIMETYPE :: ${bloc.runtimeType} $change');
   }
 }
